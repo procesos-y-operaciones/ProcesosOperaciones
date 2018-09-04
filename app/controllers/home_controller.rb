118be0d5dbf95acffc259f1124ba38321a754d87
joagranadosme <joagranadosme@unal.edu.co>
2018-09-04 16:57:27 -0500

    Defining roles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
 
   def index
+    case current_user.role
+    when 1
+      render :admin
+    when 2
+      render :user
+    else
+      render :default
+    end
   end
 
 end

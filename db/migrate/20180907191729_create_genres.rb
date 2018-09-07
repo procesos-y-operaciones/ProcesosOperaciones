1d6eee422e07f71d7b2145c47268e5ba167a5c47
joagranadosme <joagranadosme@unal.edu.co>
2018-09-07 15:21:56 -0500

    Adding data in seeds

--- db/migrate/20180907191729_create_genres.rb
+++ db/migrate/20180907191729_create_genres.rb
@@ -2,6 +2,9 @@ class CreateGenres < ActiveRecord::Migration[5.1]
   def change
     create_table :genres do |t|
 
+      t.string  :name
+      t.string  :code
+
       t.timestamps
     end
   end
--- db/migrate/20180907191740_create_generation_ranges.rb
+++ db/migrate/20180907191740_create_generation_ranges.rb
@@ -2,6 +2,9 @@ class CreateGenerationRanges < ActiveRecord::Migration[5.1]
   def change
     create_table :generation_ranges do |t|
 
+      t.string  :name
+      t.string  :code
+
       t.timestamps
     end
   end

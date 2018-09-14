8bd732e584a1e8ab2343b8fc3b50fdf28c30fd93
joagranadosme <joagranadosme@unal.edu.co>
2018-09-14 16:04:21 -0500

    Finishing areas section

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -30,7 +30,7 @@ class AreasController < ApplicationController
 
     respond_to do |format|
       if @area.save
-        format.html { redirect_to @area, notice: 'Area was successfully created.' }
+        format.html { redirect_to areas_path, notice: 'Área creada correctamente.' }
         format.json { render :show, status: :created, location: @area }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class AreasController < ApplicationController
   def update
     respond_to do |format|
       if @area.update(area_params)
-        format.html { redirect_to @area, notice: 'Area was successfully updated.' }
+        format.html { redirect_to areas_path, notice: 'Área editada correctamente.' }
         format.json { render :show, status: :ok, location: @area }
       else
         format.html { render :edit }
@@ -58,7 +58,7 @@ class AreasController < ApplicationController
   def destroy
     @area.destroy
     respond_to do |format|
-      format.html { redirect_to areas_url, notice: 'Area was successfully destroyed.' }
+      format.html { redirect_to areas_url, notice: 'Área borrada correctamente.' }
       format.json { head :no_content }
     end
   end
@@ -71,6 +71,6 @@ class AreasController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def area_params
-      params.fetch(:area, {})
+      params.require(:area).permit(:code, :name, :areas_id)
     end
 end

70366b435aa394eb36d63217f2185c16ec2b84c4
joagranadosme <joagranadosme@unal.edu.co>
2018-11-16 12:18:56 -0500

    Fixing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( application.sass )
 Rails.application.config.assets.precompile += %w( areas.js.coffee )
 Rails.application.config.assets.precompile += %w( charges.js.coffee )
 Rails.application.config.assets.precompile += %w( departaments.js.coffee )

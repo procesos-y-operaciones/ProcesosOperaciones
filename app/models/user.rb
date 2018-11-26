73b0f8838b2239094df75795c265fdd613b9a877
joagranadosme <joagranadosme@unal.edu.co>
2018-11-26 11:11:49 -0500

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => Rails.root.join('public','images',':style','missing.png')
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :area
